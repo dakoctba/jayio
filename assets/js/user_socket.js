b10ea38f6900859eaf0737d32f1468d2c4913ac0
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2022-06-03 19:26:12 -0300

    multicast

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -56,11 +56,12 @@ socket.connect()
 // Now that you are connected, you can join channels with a topic.
 // Let's assume you have a channel with a topic named `room` and the
 // subtopic is its id - in this case 42:
-let channel = socket.channel("tasks:lobby", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully at the websocket", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-channel.on('update_task', console.log);
+// let channel = socket.channel("tasks:lobby", {})
+// channel.join()
+//   .receive("ok", resp => { console.log("Joined successfully at the websocket", resp) })
+//   .receive("error", resp => { console.log("Unable to join", resp) })
+
+// channel.on('update_task', console.log);
 
 export default socket
